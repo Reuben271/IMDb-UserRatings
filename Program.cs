96db248b4a62e8341ea3b5c4ad7a0c1c4cd24af1
Archit Srivastava <rchtkumar5@gmail.com>
2022-04-19 19:35:29 +0530

    VAriables renamed

--- Program.cs
+++ Program.cs
@@ -57,6 +57,11 @@ void ScrapingDataWrite(string primaryurlappend, string baseurl)
         htmlDoc.LoadHtml(response);
         primaryurlappend = htmlDoc.DocumentNode.SelectSingleNode(".//a[@class='flat-button lister-page-next next-page']")
             .GetAttributeValue("href", string.Empty);
+
+        if (Equals(primaryurlappend, "#"))
+        {
+            break;
+        }
     }
 }
 
@@ -64,7 +69,7 @@ Dictionary<string, List<string>> ParseHtml(string html)
 {
     HtmlDocument htmlDoc = new HtmlDocument();
     htmlDoc.LoadHtml(html);
-    var programmerLinks = htmlDoc.DocumentNode.Descendants("div")
+    var nodeLinks = htmlDoc.DocumentNode.Descendants("div")
             .Where(node => node.GetAttributeValue("class", "").Contains("lister-item-content")).ToList();
 
     Dictionary<string, List<string>> parentdictionary = new Dictionary<string, List<string>>();
@@ -73,7 +78,7 @@ Dictionary<string, List<string>> ParseHtml(string html)
     List<string> ratings = new List<string>();
     List<string> userratings = new List<string>();
 
-    foreach (var link in programmerLinks)
+    foreach (var link in nodeLinks)
     {
         var titlenode = link.SelectSingleNode(".//h3");
         string titlename = titlenode.SelectSingleNode(".//a").InnerText;
@@ -94,23 +99,23 @@ Dictionary<string, List<string>> ParseHtml(string html)
     return parentdictionary;
 }
 
-void WriteToCsv(Dictionary<string, List<string>> links)
+void WriteToCsv(Dictionary<string, List<string>> mainDictData)
 {
     var csv = new StringBuilder();
-    List<string> DataFeederSize = links["titlelist"];
+    List<string> DataFeederSize = mainDictData["titlelist"];
 
     MovieContext _context = new MovieContext();
     _context.Database.EnsureCreated();
 
     for (int i = 0; i < DataFeederSize.Count; i++)
     {
-        var newLine = string.Format("{0},{1},{2}", links["titlelist"][i], links["ratinglist"][i], links["userratinglist"][i]);
-        csv.AppendLine(newLine);
-        var moviemaster = new MovieMaster { MovieName = links["titlelist"][i], Userrating = links["ratinglist"][i], Rating = links["userratinglist"][i] };
+        var csvFeeder = string.Format("{0},{1},{2}", mainDictData["titlelist"][i], mainDictData["ratinglist"][i], mainDictData["userratinglist"][i]);
+        csv.AppendLine(csvFeeder);
+        var moviemaster = new MovieMaster { MovieName = mainDictData["titlelist"][i], Userrating = mainDictData["ratinglist"][i], Rating = mainDictData["userratinglist"][i] };
         _context.MovieMaster.Add(moviemaster);
     }
     _context.SaveChanges();
-    System.IO.File.AppendAllText("links.csv", csv.ToString());
+    System.IO.File.AppendAllText("MovieMaster.csv", csv.ToString());
 }
 
 app.UseHttpsRedirection();
